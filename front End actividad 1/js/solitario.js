6fda3631103e5aeef381d72f936eeb0d89e4150d
Georgiy Kalmutskyy Kalmutskyy <79171526+kurtco1996@users.noreply.github.com>
2022-02-02 21:54:32 +0100

    REVISION 3

--- front End actividad 1/js/solitario.js
+++ front End actividad 1/js/solitario.js
@@ -3,9 +3,9 @@
 // Array de palos
 let palos = ["viu", "cua", "hex", "cir"];
 // Array de número de cartas
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // En las pruebas iniciales solo se trabajará con cuatro cartas por palo:
-//let numeros = [10, 11, 12];
+let numeros = [10, 11, 12];
 
 // paso (top y left) en pixeles de una carta a la siguiente en un mazo
 let paso = 5;
